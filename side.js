d94041edc3794727f26755cfb1a71bbac385800b
Asahi <76908462+Asahi-a@users.noreply.github.com>
2022-11-25 16:34:13 +0900

    Update side.js

--- side.js
+++ side.js
@@ -1,4 +1,24 @@
 let sidebar = document.getElementById('side')
 sidebar.innerHTML = '
 
+<div style="background-color:#000;">
+    <div class="indx">
+        <p>Menu</p>
+    </div>
+    <p style="text-align:center;" class="link"><a href="jititai.html">自治体</a></p>
+    <p style="text-align:center;" class="link">鉄道</p>
+    <p style="text-align:center;" class="link">店</p>
+    <p style="text-align:center;" class="link">人</p>
+    <p style="text-align:center;" class="link">その他</p>
+    <p style="text-align:center;" class="link">番外編/猫鯖</p>
+    <p></p>
+    <div class="indx" style="margin-bottom:2px;">
+        <p>砂漠の砂漠ﾐﾈﾗﾙｳｫｰﾀｰ</p>
+    </div>
+    <img src="IMG_3243.PNG" class="bardelighter"></img>
+    <p>↑鉄分含有/MOD不使用</p>
+    <!-- bardeliteghterはinflameの中に入れたときに横とか下にずらせない状況にしたいのでcssでwidthを100%にできるよう中に画像入れる場合は個別に設置。
+ もし1000pxを超す場合には必ずcssも更新すること-->
+</div>
+
 ';
