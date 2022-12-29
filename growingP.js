608068b41a4a68462584d7870bfa538932bc507b
Asahi <76908462+Asahi-a@users.noreply.github.com>
2022-12-29 23:19:22 +0900

    Create growingP.js

---
+++ growingP.js
@@ -0,0 +1,9 @@
+let growingP = doccument.getElementById('p');
+
+growingP.addEventListener('mouseover', function() {
+  element.style.color = '#9FF';
+});
+
+growingP.addEventListener('mouseout', function() {
+  element.style.color = '#FD8';
+});
