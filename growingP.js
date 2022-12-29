03880848ff905fc7bd9fdbbb150b9fd24521584a
Asahi <76908462+Asahi-a@users.noreply.github.com>
2022-12-30 00:02:33 +0900

    Update growingP.js

--- growingP.js
+++ growingP.js
@@ -1,9 +1,24 @@
-let growingP = document.getElementById('p');
+let growingP = document.getElementsByTagName('p');
+//フォントサイズによって相対的に変えたいので一度cssから数値として取得して演算して変更する
+let cssP = getComputedStyle('growingP');
+let baseSize = cssP.getPropertyValue("font-size");
+let mouseoverSize = cssP.getPropertyValue("font-size") * 1.5;
 
+//変更する
 growingP.addEventListener('mouseover', function() {
+  element.style.fontSize = mouseoverSize + 'px';
+});
+
+growingP.addEventListener('mouseout', function() {
+  element.style.fontSize = baseSize + 'px';
+});
+
+
+
+/*growingP.addEventListener('mouseover', function() {
   element.style.color = '#9FF';
 });
 
 growingP.addEventListener('mouseout', function() {
   element.style.color = '#FD8';
-});
+});*/
