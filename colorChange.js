80b3b04f6c809b2b71de227a35fa447b2ca77509
Asahi <76908462+Asahi-a@users.noreply.github.com>
2022-12-31 12:22:35 +0900

    Update colorChange.js

--- colorChange.js
+++ colorChange.js
@@ -1,1 +1,12 @@
+var change = document.getElementById('change');
 
+var white = document.getElementById('whiteout');
+var black = document.getElementById('blackout');
+
+white.addEventListener('click',function(){
+  change.innnerHTML ='body {background-color: #fff;font-family: serif;color: #000;font-size:16px;}.main-contents {width: 70%;padding: 10px;background-color:#000;}.middle {background-color: #000;width: 1000px;margin: auto;border-left: 1px solid;border-right: 1px solid;border-color: #aaa;}.hd {text-align: center;background-color:#000;}';
+});
+
+black.addEventListener('click',function(){
+  change.innnerHTML ='.asdfgh{color:#000;}';
+});
